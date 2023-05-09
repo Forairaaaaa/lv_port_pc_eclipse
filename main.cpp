2301efadcea54b9712e9cddc6ef1a33fadaa2af3
Forairaaaaa <applesyqd@outlook.com>
2023-05-09 18:19:05 +0800

    .

--- main.cpp
+++ main.cpp
@@ -14,70 +14,95 @@
 #include "lvgl/examples/lv_examples.h"
 #include "lvgl/demos/lv_demos.h"
 
-
-#include <app/app_manager.h>
-
-MOONCAKE::APP_Manger app_manager;
-
-
-/*********************
- *      DEFINES
- *********************/
-
-/**********************
- *      TYPEDEFS
- **********************/
-
-/**********************
- *  STATIC PROTOTYPES
- **********************/
 static lv_disp_t * hal_init(lv_coord_t w, lv_coord_t h);
 
-/**********************
- *  STATIC VARIABLES
- **********************/
 
-/**********************
- *      MACROS
- **********************/
 
-/**********************
- *   GLOBAL FUNCTIONS
- **********************/
+#include <iostream>
 
-/*********************
- *      DEFINES
- *********************/
+// #include <app/app_manager.h>
+#include <framework/framework.h>
+#include <SDL2/SDL.h>
 
-/**********************
- *      TYPEDEFS
- **********************/
 
-/**********************
- *      VARIABLES
- **********************/
 
-/**********************
- *  STATIC PROTOTYPES
- **********************/
+MOONCAKE::Framework app_manager;
 
-/**********************
- *   GLOBAL FUNCTIONS
- **********************/
+
+
+
+class testApp1 : public MOONCAKE::APP_BASE {
+    public:
+        testApp1(const char* name) {
+            setAppName(name);
+        }
+        void onSetup() {
+            setAllowBgRunning(true);
+            printf("%s onSetup\n", getAppName().c_str());
+        }
+        void onCreate() {
+            printf("%s onCreate\n", getAppName().c_str());
+            // lv_demo_widgets();
+            lv_demo_benchmark(LV_DEMO_BENCHMARK_MODE_REAL);
+        }
+        void onResume() {
+            printf("%s onResume\n", getAppName().c_str());
+        }
+        void onRunning() {
+            printf("%s onRunning\n", getAppName().c_str());
+        }
+        void onRunningBG() {
+            printf("%s onRunningBG\n", getAppName().c_str());
+        }
+        void onPause() {
+            printf("%s onPause\n", getAppName().c_str());
+        }
+        void onDestroy() {
+            printf("%s onDestroy\n", getAppName().c_str());
+        }
+};
+
+class testApp2 : public MOONCAKE::APP_BASE {
+    public:
+        testApp2(const char* name) {
+            setAppName(name);
+            setAllowBgRunning(true);
+        }
+        void onSetup() {
+            printf("%s onSetup\n", getAppName().c_str());
+        }
+        void onCreate() {
+            printf("%s onCreate\n", getAppName().c_str());
+        }
+        void onResume() {
+            printf("%s onResume\n", getAppName().c_str());
+        }
+        void onRunning() {
+            printf("%s onRunning\n", getAppName().c_str());
+        }
+        void onRunningBG() {
+            printf("%s onRunningBG\n", getAppName().c_str());
+        }
+        void onPause() {
+            printf("%s onPause\n", getAppName().c_str());
+        }
+        void onDestroy() {
+            printf("%s onDestroy\n", getAppName().c_str());
+        }
+};
+
+
 
 int main(int argc, char **argv)
 {
-  (void)argc; /*Unused*/
-  (void)argv; /*Unused*/
 
-  /*Initialize LVGL*/
   lv_init();
 
   /*Create a default group for keyboard navigation*/
   lv_group_set_default(lv_group_create());
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  hal_init(800, 480);
+  hal_init(480, 320);
 
 //  lv_example_switch_1();
 //  lv_example_calendar_1();
@@ -97,14 +122,106 @@ int main(int argc, char **argv)
 //  lv_example_flex_3();
 //  lv_example_label_1();
 
-  lv_demo_widgets();
+//   lv_demo_widgets();
 
-  while(1) {
-      /* Periodically call the lv_task handler.
-       * It could be done in a timer interrupt or an OS task too.*/
-      lv_timer_handler();
-      usleep(5 * 1000);
-  }
+
+    
+    
+
+
+    std::cout << app_manager.init() << "\n";
+
+
+    while (1) {
+        app_manager.update();
+        lv_timer_handler();
+
+        usleep(200 * 1000);
+    }
+
+
+
+
+    MOONCAKE::APP_BASE* test_app_A = new testApp1("[AAA]");
+    MOONCAKE::APP_BASE* test_app_B = new testApp2("[BBB]");
+    MOONCAKE::APP_BASE* test_app_C = new testApp2("[CCC]");
+
+    std::cout << "install-----\n";
+    app_manager.install(test_app_A, nullptr);
+    app_manager.install(test_app_B, nullptr);
+    app_manager.install(test_app_C, nullptr);
+
+
+    std::cout << "start-----\n";
+    app_manager.startApp(test_app_A);
+    app_manager.startApp(test_app_B);
+    app_manager.startApp(test_app_C);
+    // app_manager.closeApp(test_app_C);
+    // app_manager.destroyApp(test_app_C);
+    // app_manager.closeApp(test_app_C);
+    // app_manager.destroyApp(test_app_C);
+
+    /* 2 app test */
+    int count = 0;
+    while (1)
+    {
+        lv_timer_handler();
+
+
+
+
+        app_manager.update();
+        usleep(5 * 1000);
+        // usleep(200 * 1000);
+
+        count++;
+
+        if (count == 4) {
+            // std::cout << app_manager.closeApp(test_app_1) << "\n";
+            // app_manager.closeApp(test_app_A);
+            app_manager.closeApp(test_app_B);
+            std::cout << "444-----\n";
+        }
+        if (count == 8) {
+            // std::cout << app_manager.startApp(test_app_1) << "\n";
+            app_manager.startApp(test_app_A);
+
+            std::cout << "888-----\n";
+        }
+        if (count == 12) {
+            // std::cout << app_manager.destroyApp(test_app_1) << "\n";
+            app_manager.destroyApp(test_app_A);
+            app_manager.destroyApp(test_app_B);
+            app_manager.startApp(test_app_C);
+            app_manager.destroyAllApps();
+            std::cout << "121212-----\n";
+        }
+        printf("\n");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   while(1) {
+//       /* Periodically call the lv_task handler.
+//        * It could be done in a timer interrupt or an OS task too.*/
+//       lv_timer_handler();
+//       usleep(5 * 1000);
+//   }
 
   return 0;
 }
